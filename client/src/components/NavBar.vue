7f29898359c7d5929dad4b0c7d0b19c05336b669
Pippin404 <Baconpippin@gmail.com>
2024-03-09 17:54:34 -0500

    changed the users to correct navbar location

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -61,7 +61,7 @@ function toggleMenu() {
       </span>
       </RouterLink>
 
-      <RouterLink to="/products" class="navbar-item">
+      <RouterLink to="/" class="navbar-item">
         <span class="icon-text">
         <span class="icon">
           <i class="fas fa-search"></i>
@@ -76,7 +76,7 @@ function toggleMenu() {
         </a>
 
         <div class="navbar-dropdown">
-          <RouterLink to="/about" class="navbar-item">
+          <RouterLink to="/products" class="navbar-item">
             Users
           </RouterLink>
         </div>
