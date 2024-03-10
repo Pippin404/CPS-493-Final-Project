fcb2478b96663960fa8f037c72c169a5b0b44fcf
Pippin404 <Baconpippin@gmail.com>
2024-03-09 19:04:47 -0500

    Fixed statistic page, Preparing for dummy data

--- client/src/pages/Statistics.vue
+++ client/src/pages/Statistics.vue
@@ -1,7 +1,85 @@
 <template>
     <div class="Stats">
-      <h1>Extra Credit</h1>
-      <h3>You don't need a comprehensive statistics page. The simple statistics on the home page will do!</h3>
+      <div class="columns">
+        <!--Card 1-->
+        <div class="column column is-half is-offset-one-quarter">
+          <div class="card">
+            <div class="card-content">
+              <p class="title">
+                Today
+              </p>
+  
+  
+              <p class="subtitle value">
+                0 Ft
+              </p>
+              <p class="subtitle value">
+                0 Calories
+              </p>
+              <p class="subtitle value r">
+                Duration: 0 min
+              </p>
+              <p class="subtitle value r">
+                Avr. Pace: 0ft
+              </p>
+  
+  
+              
+            </div>
+          </div>
+  
+          <div class="card">
+            <div class="card-content">
+              <p class="title">
+                This Week
+              </p>
+  
+  
+              <p class="subtitle value">
+                0 Ft
+              </p>
+              <p class="subtitle value">
+                0 Calories
+              </p>
+              <p class="subtitle value r">
+                Duration: 0 min
+              </p>
+              <p class="subtitle value r">
+                Avr. Pace: 0ft
+              </p>
+  
+  
+              
+            </div>
+          </div>
+  
+          <div class="card">
+            <div class="card-content">
+              <p class="title">
+                All Time
+              </p>
+  
+  
+              <p class="subtitle value">
+                0 Ft
+              </p>
+              <p class="subtitle value">
+                0 Calories
+              </p>
+              <p class="subtitle value r">
+                Duration: 0 min
+              </p>
+              <p class="subtitle value r">
+                Avr. Pace: 0ft
+              </p>
+  
+  
+              
+            </div>
+          </div>
+  
+        </div>
+      </div>
     </div>
   </template>
   
