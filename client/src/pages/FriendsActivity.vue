b1b671cc98078f952558834512fefd20c0269b15
Pippin404 <Baconpippin@gmail.com>
2024-03-11 17:00:35 -0400

    Added form control

--- client/src/pages/FriendsActivity.vue
+++ client/src/pages/FriendsActivity.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-let isCollapsed = ref(false);
-let showPopup = ref(false);
+const isCollapsed = ref(false);
+const showPopup = ref(false);
 
+const newActivity = ref<Root | null>(null);
 
 //Import time baby
 import { RouterLink } from 'vue-router';
@@ -26,6 +27,28 @@ onMounted(async () => {
 function isMatchingEmail(userEmail: string, productEmail: string): boolean {
   return userEmail === productEmail;
 }
+
+function addActivity() {
+    newActivity.value = {
+        email: '',
+        place: '',
+        distance: 0,
+        duration: 0,
+        date: '',
+        type: '',
+        image: '',
+    }
+}
+
+function saveActivity(){
+
+    if (newActivity.value) {
+        products.value.unshift(newActivity.value);
+        newActivity.value = null;
+    }
+
+
+}
 </script>
 
 <template>
@@ -35,62 +58,65 @@ function isMatchingEmail(userEmail: string, productEmail: string): boolean {
             <div class="column is-two-thirds">
                 <h1 class="title">Friends activity</h1>
 
-        <button class="button is-medium is-fullwidth is-primary" @click="showPopup = true">Add Activity</button>
+        <button class="button is-medium is-fullwidth is-primary" @click="addActivity">Add Activity</button>
 
-        <div v-if="showPopup" class="modal is-active">
+
+
+
+        <div v-if="newActivity" class="modal is-active">
             <div class="modal-background"></div>
             <div class="modal-card">
                 <header class="modal-card-head">
                     <p class="modal-card-title">Add Activity</p>
-                    <button class="delete" aria-label="close" @click="showPopup = false"></button>
+                    <button class="delete" aria-label="close" @click="newActivity=null"></button>
                 </header>
                 <section class="modal-card-body">
-                    <form>
+                    <form @submit.prevent="saveActivity">
                         <div class="field">
                             <label class="label">Email</label>
                             <div class="control">
-                                <input class="input" type="email" placeholder="Enter your email">
+                                <input class="input" type="email" placeholder="Enter your email" v-model="newActivity.email">
                             </div>
                         </div>
                         <div class="field">
                             <label class="label">Place</label>
                             <div class="control">
-                                <input class="input" type="text" placeholder="Enter the place">
+                                <input class="input" type="text" placeholder="Enter the place" v-model="newActivity.place">
                             </div>
                         </div>
                         <div class="field">
                             <label class="label">Distance</label>
                             <div class="control">
-                                <input class="input" type="text" placeholder="Enter the distance">
+                                <input class="input" type="text" placeholder="Enter the distance" v-model="newActivity.distance">
                             </div>
                         </div>
                         <div class="field">
                             <label class="label">Duration</label>
                             <div class="control">
-                                <input class="input" type="text" placeholder="Enter the duration">
+                                <input class="input" type="text" placeholder="Enter the duration" v-model="newActivity.duration">
                             </div>
                         </div>
                         <div class="field">
                             <label class="label">Date</label>
                             <div class="control">
-                                <input class="input" type="date" placeholder="Enter the date">
+                                <input class="input" type="date" placeholder="Enter the date" v-model="newActivity.date">
                             </div>
                         </div>
                         <div class="field">
                             <label class="label">Type</label>
                             <div class="control">
-                                <input class="input" type="type" placeholder="What kind?">
+                                <input class="input" type="type" placeholder="What kind?" v-model="newActivity.type">
                             </div>
                         </div>
                         <div class="field">
                             <label class="label">Image</label>
                             <div class="control">
-                                <input class="input" type="text" placeholder="Enter the image URL">
+                                <input class="input" type="text" placeholder="Enter the image URL" v-model="newActivity.image">
                             </div>
                         </div>
                         <div class="field">
                             <div class="control">
-                                <button class="button is-primary">Submit</button>
+                                <input class="button is-primary" type="submit" value="Save">
                             </div>
                         </div>
                     </form>
