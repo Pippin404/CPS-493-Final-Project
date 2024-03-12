e3532a3d10a6f47efef00c0defc870f5c44b0b1b
Pippin404 <Baconpippin@gmail.com>
2024-03-12 19:24:15 -0400

    Added the ActivityList Component to the components list for better FriendsActivity readablity

--- client/src/pages/FriendsActivity.vue
+++ client/src/pages/FriendsActivity.vue
@@ -3,6 +3,7 @@ import { RouterLink } from 'vue-router';
 import { ref, computed, onMounted } from 'vue';
 import { type ActivityData, getActivity } from "@/model/products";
 import { getUsers, type UserRoot } from '@/model/users';
+import ActivityList from '@/components/ActivityList.vue';
 
 const isCollapsed = ref(false);
 const showPopup = ref(false);
@@ -57,9 +58,10 @@ function saveActivity(){
             <div class="column is-two-thirds">
                 <h1 class="title">Friends activity</h1>
 
+
+
         <button class="button is-medium is-fullwidth is-primary" @click="addActivity">Add Activity</button>
-
-
+        
         <div v-if="newActivity" class="modal is-active">
             <div class="modal-background"></div>
             <div class="modal-card">
@@ -120,52 +122,12 @@ function saveActivity(){
                 </section>
             </div>
         </div>
+
+
       
 
-
-
-
-
-        <div class="product-list">
-            <div v-for="product in products" :key="product.email" class="card">
-                <div class="card-image">
-                    <div class="card-content">
-                        <button class="delete" @click="removeCard(product)">Delete</button>
-
-                        <!--Display a persons info-->
-                        <div class="user-list">
-                        <div v-for="user in users" :key="user.email" class="card">
-
-                            <div class="userInfo" v-if="isMatchingEmail(user.email, product.email)">
-                                <img :src="user.image" class="userImage" alt="Product image" v-if="isMatchingEmail(user.email, product.email)">
-                                {{ user.firstName }}
-                            </div>
-                            <div class="userInfo" v-if="isMatchingEmail(user.email, product.email)">
-                                {{  }}
-                            </div>
-                            
-                        </div>  
-                    </div>
-
-                    <!--Other Card Content-->
-                    <h3 class="subtitle is-6"> At {{ product.date }}</h3>
-                    <br>
-                    <img :src="product.image" alt="Product image" class="small">
-                    <p class="subtitles">{{ product.type }} thru {{ product.place }}</p>
-                    <p class="subtitles"> Distance: {{ product.distance }} Meters in {{ product.duration }} Minutes</p>
-                    <p class="subtitles"> Date: {{ product.date }}</p>  
-                    <div class="hoverable-icons">
-                        <i class="fas fa-reply"></i>
-                        <i class="fas fa-retweet"></i>
-                        <i class="fas fa-heart"></i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-
+        <ActivityList>
+        </ActivityList>
 
 
         </div>
@@ -173,48 +135,5 @@ function saveActivity(){
 </template>
 <style scoped>
 
-    .small{
-        width: 30%;
-        margin: 30px;
-        float: right;
-    }
-    .userName{
-        font-weight: bold;
-        color:black;
-        font-size: 30px;
-        
-    }
-    .card{
-        margin-top: 20px;
-    }
-
-    .subtitles{
-        font-size: 20px;
-        color:black;
-        margin: 10px;
-    }
-    .delete {
-        margin-left: auto;
-    }
     
-    .hoverable-icons{
-        margin: 5px;
-    }
-    .fas{
-        margin: 5px;
-        cursor: pointer;
-        color: #00d1b2;
-    }
-
-    .userInfo{
-        font-size: 30px;
-        color:black;
-        margin: 10px;
-    }
-
-    .userImage{
-
-        height: 40px;
-
-    }
 </style>
