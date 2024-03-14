58319b12a2f52c090280a624f8e190455e0be667
Pippin404 <Baconpippin@gmail.com>
2024-03-14 00:26:03 -0400

    Added Activity page that displays only when signed in

--- client/src/components/UserList.vue
+++ client/src/components/UserList.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-     import { ref, computed, onMounted } from 'vue';
+    import { ref, computed, onMounted } from 'vue';
     import { type UserRoot, getUsers } from "@/model/users";
     const products = ref([] as UserRoot[]);
 
--- client/src/pages/Activity.vue
+++ client/src/pages/Activity.vue
@@ -1,40 +1,62 @@
 <script setup lang="ts">
+  import { getCurrentUser } from '@/model/currentUser';import { RouterLink } from 'vue-router';
+import { ref, computed, onMounted } from 'vue';
+import { type ActivityData, getActivity } from "@/model/products";
+
+const Activities = ref([] as ActivityData[]);
+
+onMounted(async () => {
+    Activities.value = await getActivity();
+});
+
 </script>
 
 <template>
-    <section class="hero">
-  <div class="hero-body">
-    <p class="title">
-        Login
-    </p>
-    <p class="subtitle">
-        In the meantime you can login using the login menu on the top right.
-    </p>
-    <p>
-        Logging in as Moshe should give you access to the Activity page as well as the admin pages (right now just users). Logging in as other should give you only the Activity page
-    </p>
+
+  <!--Only if no login-->
+  <section class="hero" v-if="getCurrentUser() ==''">
+    <div class="hero-body"></div>
+      <div class="hero-body">
+        <p class="title">
+            Login
+        </p>
+        <p class="subtitle">
+            In the meantime you can login using the login menu on the top right.
+        </p>
+      </div>
+    </section>
 
 
-    <!--Loops thru and displays the users Activities IF I CAN GET THE FUCKING PROFILE DATA FROM THE NAVBAR-->
+    <section class="hero" v-if="getCurrentUser() !==''">
+      <div class="hero-body">
+        <p class="title">
+          Here is your Activity!
+        </p>
+        
+
+        <div v-for="activity in Activities" :key="activity.email" class="card">
+          <div class="card-content">
+
+            <div v-if="activity.email == getCurrentUser()">
+              <h1>{{ activity.email }}</h1>
+              <h3 class="subtitle is-6"> At {{ activity.date }}</h3>
+              <br>
+              <img :src="activity.image" alt="activity image" class="small">
+              <p class="subtitles">{{ activity.type }} thru {{ activity.place }}</p>
+              <p class="subtitles"> Distance: {{ activity.distance }} Meters in {{ activity.duration }} Minutes</p>
+              <p class="subtitles"> Date: {{ activity.date }}</p>
+          </div> 
+          </div>
+        </div>
 
 
 
 
-
-
-  </div>
-</section>
-    
-
-
-
-
-
-    
-      
-    
+        
+      </div>
+    </section>
 </template>
 
 <style scoped>
-
+  
 </style>
